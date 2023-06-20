c6010db01cc41a9aabb4c9b8623f79a904ad6f74
roo123fre <kibichdaneil@gmail.com>
2023-06-20 13:24:04 +0300

    added adaptive part one

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new Swiper('.hero__slider', {
+    slidesPerView: 2,
+    spaceBetween:10,
+    loop:true,
+    navigation: {
+        prevEl: 'hero__slider-btn_prev',
+        nextEl:'hero__slider-btn_next',
+    },
+    autoplay:{
+        delay: 3000,
+    }
+})
